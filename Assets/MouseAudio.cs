bb7d6a256ca7b4df4e3d7f7ddff485851e0b97a8
Dk <70618651+kiuuu00@users.noreply.github.com>
2020-10-09 23:51:09 -0600

    W6A

---
+++ Assets/MouseAudio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAudio : MonoBehaviour{
+    public AudioSource AudioS;
+
+    public void PlayClip(AudioClip clip)
+    {
+        AudioS.PlayOneShot(clip);
+    }
+}
